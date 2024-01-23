b60e8b583e5bac49e36c22019ff03f492f5c4c6a
yolo <igoncharov@flightpath3d.com>
2024-01-23 16:44:41 +0000

    feat(fs): implement list.js
    implement function that prints all array of filenames from files folder into console (if files folder doesn't exists Error with message FS operation failed must be thrown)

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { readdir } from 'node:fs/promises';
+import { dirname } from 'path';
+
 const list = async () => {
-    // Write your code here 
+	try {
+		const files = await readdir(`${dirname(import.meta.filename)}/files`);
+
+		for (const file of files)
+			console.log(file);
+	} catch (err) {
+		throw new Error('FS operation failed');
+	}
 };
 
 await list();
