fb0c4cde664b652ec639b4b6edad543f3e52ebd7
igoncharov <igoncharov@flightpath3d.com>
2024-01-23 22:33:27 +0000

    feat(cli): implement args.js
    implement function that parses command line arguments (given in format --propName value --prop2Name value2, you don't need to validate it) and prints them to the console in the format propName is value, prop2Name is value2

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const pairs = []
+  
+  process.argv.slice(2).forEach((arg) => {
+    if(arg.startsWith('--')) {
+      pairs.push({key: arg.slice(2), value: null})
+    } else if(pairs.length) {
+      pairs[pairs.length - 1].value = arg
+    }
+  })
+  
+  console.log(pairs.map(({key, value}) => `${key} is ${value}`).join(', '));
 };
 
 parseArgs();
