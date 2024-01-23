fde0621dbeeea9fdbe789c166fccf1fbe9e12427
yolo <igoncharov@flightpath3d.com>
2024-01-23 16:31:56 +0000

    feat(fs): implement rename.js
    implement function that renames file wrongFilename.txt to properFilename with extension .md (if there's no file wrongFilename.txt or properFilename.md already exists Error with message FS operation failed must be thrown)

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import isPathExists from './helpers/isPathExists.js';
+import { dirname } from 'path';
+import { rename as fsRename } from 'node:fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+  const from = `${dirname(import.meta.filename)}/files/wrongFilename.txt`;
+  const to = `${dirname(import.meta.filename)}/files/properFilename.md`;
+
+  if (await isPathExists(to) || !(await isPathExists(from))) {
+    throw new Error('FS operation failed');
+  } else {
+    await fsRename(from, to);
+  }
 };
 
 await rename();
