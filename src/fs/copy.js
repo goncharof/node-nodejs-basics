0188043e37069aafb4f0a87271e334287bd712c9
yolo <igoncharov@flightpath3d.com>
2024-01-23 15:44:31 +0000

    feat(fs): implement copy.js
    implement function that copies folder files files with all its content into folder files_copy at the same level (if files folder doesn't exists or files_copy has already been created Error with message FS operation failed must be thrown)

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import { dirname } from 'path';
+import { cp } from 'node:fs/promises';
+import isPathExists from './helpers/isPathExists.js';
+
 const copy = async () => {
-    // Write your code here 
+  const dirSource = `${dirname(import.meta.filename)}/files`;
+  const dirDestination = `${dirname(import.meta.filename)}/files_copy`;
+
+  if ((await isPathExists(dirDestination)) || !(await isPathExists(dirSource))) {
+    throw new Error('FS operation failed');
+  }
+
+  await cp(dirSource, dirDestination, { recursive: true });
 };
 
 await copy();
