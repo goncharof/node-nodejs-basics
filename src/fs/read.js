6873ad29279766b24ad88cc88a3da7290e304118
yolo <igoncharov@flightpath3d.com>
2024-01-23 16:51:12 +0000

    feat(fs): implement read.js
    implement function that prints content of the fileToRead.txt into console (if there's no file fileToRead.txt Error with message FS operation failed must be thrown)

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { dirname } from 'path';
+import { readFile } from 'node:fs/promises';
+
 const read = async () => {
-    // Write your code here 
+  const filename = `${dirname(import.meta.filename)}/files/fileToRead.txt`;
+
+  try {
+    const contents = await readFile(filename, 'utf8');
+    console.log(contents);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
