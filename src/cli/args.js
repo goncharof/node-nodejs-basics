eda0c0aad31474df7190ddff8d533b93a62f2d50
igoncharov <igoncharov@flightpath3d.com>
2024-01-23 22:46:00 +0000

    docs: only add some comments

--- src/cli/args.js
+++ src/cli/args.js
@@ -3,12 +3,13 @@ const parseArgs = () => {
   
   process.argv.slice(2).forEach((arg) => {
     if(arg.startsWith('--')) {
-      pairs.push({key: arg.slice(2), value: null})
+      pairs.push({key: arg.slice(2), value: null}) // because there can be no value associated with key
     } else if(pairs.length) {
-      pairs[pairs.length - 1].value = arg
+      pairs[pairs.length - 1].value = arg // add value to previous key
     }
   })
   
+  // finally print
   console.log(pairs.map(({key, value}) => `${key} is ${value}`).join(', '));
 };
 
