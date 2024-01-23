e46582c2c3216fa412e0d00aaa59b1b77ec4ba22
yolo <igoncharov@flightpath3d.com>
2024-01-23 15:20:56 +0000

    feat(fs): implement create.js
    create.js - implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder (if file already exists Error with message FS operation failed must be thrown)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile, access, constants } from 'node:fs/promises';
+import { dirname } from 'path';
+
+const checkIfFileExists = async (filename) => {
+  try {
+    await access(filename, constants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
 const create = async () => {
-    // Write your code here 
+  const filename = `${dirname(import.meta.filename)}/files/freshFile.txt`;
+
+  if (await checkIfFileExists(filename)) {
+    throw new Error('FS operation failed');
+  }
+
+  await writeFile(filename, 'I am fresh and young');
 };
 
 await create();
