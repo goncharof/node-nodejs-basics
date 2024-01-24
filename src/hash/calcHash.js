97c51e4d6c54e4a2e95b87a002a5037fec9ee522
yolo <igoncharov@flightpath3d.com>
2024-01-24 18:20:28 +0000

    feat: implement calcHash.js
     implement function that calculates SHA256 hash for file fileToCalculateHashFor.txt and logs it into console as hex using Streams API

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+import { extendedDirname } from '../helpers/__path.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const input = createReadStream(extendedDirname(import.meta.url, 'files/fileToCalculateHashFor.txt'));
+  input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
