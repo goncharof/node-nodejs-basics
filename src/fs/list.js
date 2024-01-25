2ffa82c3757a242d5ca128300bfafe871be7b163
igoncharov <igoncharov@flightpath3d.com>
2024-01-25 01:10:32 +0000

    style: more readable code

--- src/fs/list.js
+++ src/fs/list.js
@@ -3,18 +3,18 @@ import { extendedDirname } from '../helpers/__path.js';
 
 const list = async () => {
 	try {
-		const files = (await readdir(extendedDirname(import.meta.url, 'files'), { withFileTypes: true })).map(file => {
-			return {
+		const files = (await readdir(extendedDirname(import.meta.url, 'files'), { withFileTypes: true }))
+		.map(file => ({
 				name: file.name,
 				isFile: file.isFile(),
 				isDirectory: file.isDirectory()
-			};
-		});
-		console.table(files, ['name', 'isFile', 'isDirectory']);
-	} catch (err) {
-		console.log(err);
-		throw new Error('FS operation failed');
-	}
-};
-
-await list();
+			}));
+			
+			console.table(files, ['name', 'isFile', 'isDirectory']);
+		} catch (err) {
+			console.log(err);
+			throw new Error('FS operation failed');
+		}
+	};
+	
+	await list();
