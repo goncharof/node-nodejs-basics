347e41726d682fc0018cd64f767cdd9c7450a71b
igoncharov <igoncharov@flightpath3d.com>
2024-01-26 21:31:27 +0000

    refactor(hash): use pipline, error handler

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -2,11 +2,17 @@ import { createHash } from 'node:crypto';
 import { open } from 'node:fs/promises';
 import { stdout } from 'node:process';
 import { extendedDirname } from '../helpers/__path.js';
+import { pipeline } from 'node:stream';
 
 const calculateHash = async () => {
-  const hash = createHash('sha256');
-  const fd = await open(extendedDirname(import.meta.url, 'files', 'fileToCalculateHashFor.txt'), 'r');
-  fd.createReadStream().pipe(hash).setEncoding('hex').pipe(stdout);
+  pipeline(
+    (await open(extendedDirname(import.meta.url, 'files', 'fileToCalculateHashFor.txt'), 'r')).createReadStream(),
+    createHash('sha256').setEncoding('hex'),
+    stdout,
+    (err) => {
+      if (err) console.error(err)
+    }
+  )
 };
 
 await calculateHash();
