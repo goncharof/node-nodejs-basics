409f6eeac6bb228dad8368cef2f23cf9859d8e7c
igoncharov <igoncharov@flightpath3d.com>
2024-01-26 18:18:17 +0000

    feat(zlib): implement compress.js
     implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { open } from 'node:fs/promises';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { extendedDirname } from '../helpers/__path.js';
+
 const compress = async () => {
-    // Write your code here 
+  const [fdSource, fdDestination] = await Promise.all([
+    open(extendedDirname(import.meta.url, 'files', 'fileToCompress.txt'), 'r'),
+    open(extendedDirname(import.meta.url, 'files', 'archive.gz'), 'w')
+  ])
+
+  pipeline(fdSource.createReadStream(), createGzip(), fdDestination.createWriteStream(), (err) => {
+    if (err) console.error(err);
+  })
 };
 
 await compress();
