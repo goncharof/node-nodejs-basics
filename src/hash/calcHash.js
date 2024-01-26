6bd81b7113c26968047d22e11f60908290ad0cfc
igoncharov <igoncharov@flightpath3d.com>
2024-01-26 16:55:13 +0000

    feat(hash): promises api

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,12 +1,12 @@
 import { createHash } from 'node:crypto';
-import { createReadStream } from 'node:fs';
+import { open } from 'node:fs/promises';
 import { stdout } from 'node:process';
 import { extendedDirname } from '../helpers/__path.js';
 
 const calculateHash = async () => {
   const hash = createHash('sha256');
-  const input = createReadStream(extendedDirname(import.meta.url, 'files', 'fileToCalculateHashFor.txt'));
-  input.pipe(hash).setEncoding('hex').pipe(stdout);
+  const fd = await open(extendedDirname(import.meta.url, 'files', 'fileToCalculateHashFor.txt'), 'r');
+  fd.createReadStream().pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
