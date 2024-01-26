7ad5557adbbb56b8051402011bcdec9fc1c8107b
igoncharov <igoncharov@flightpath3d.com>
2024-01-26 17:30:46 +0000

    feat(streams): implement transform.js
    implement function that reads data from process.stdin, reverses text using Transform Stream and then writes it into process.stdout

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { stdin, stdout } from 'node:process';
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseStream = new Transform({
+    transform(chunk, encoding, cb) {
+      cb(null, chunk.toString().trim().split('').reverse().join('') + '\n');
+    }
+  });
+  
+  pipeline(stdin, reverseStream, stdout, (error) => console.error(error));
 };
 
 await transform();
