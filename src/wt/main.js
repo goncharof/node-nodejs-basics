41bf68403267174db790a037fb4d96273f559933
igoncharov <igoncharov@flightpath3d.com>
2024-01-27 12:24:07 +0000

    feat(wt): WIP - create worker and reciave data

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,12 @@
+import { Worker } from 'node:worker_threads';
+import { extendedDirname } from '../helpers/__path.js';
+
 const performCalculations = async () => {
-    // Write your code here
+  const worker = new Worker(extendedDirname(import.meta.url, 'worker.js'), { workerData: 10 });
+
+  worker.on('message', (result) => {
+    console.log(result);
+  })
 };
 
 await performCalculations();
