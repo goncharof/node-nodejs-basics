dada8cc092454551eedd42266ee3159431a046fd
igoncharov <igoncharov@flightpath3d.com>
2024-01-27 12:23:37 +0000

    fix(wt): worker.js - postMessage workerData

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,13 +1,11 @@
-import { parentPort } from 'node:worker_threads';
+import { parentPort, workerData } from 'node:worker_threads';
 
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
   // This function sends result of nthFibonacci computations to main thread
-  parentPort.on('message', (n) => {
-    parentPort.postMessage(nthFibonacci(n));
-  })
+  parentPort.postMessage(nthFibonacci(workerData));
 };
 
 sendResult();
