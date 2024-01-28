d5bbb80db608921c2445064dfd0e4937e9ae0b53
igoncharov <igoncharov@flightpath3d.com>
2024-01-28 21:26:32 +0000

    feat(cpt): implement cp.js
    cp.js - implement function spawnChildProcess that receives array of arguments args and creates child process from file script.js, passing these args to it. This function should create IPC-channel between stdin and stdout of master process and child process:
    child process stdin should receive input from master process stdin
    child process stdout should send data to master process stdout

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { fork } from 'node:child_process';
+import { extendedDirname } from '../helpers/__path.js';
+import { stdin, stdout } from 'node:process';
 
+const spawnChildProcess = async (args) => {
+  const cp = fork(
+    extendedDirname(import.meta.url, 'files', 'script.js'),
+    args,
+    { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] }
+  );
+    
+  stdin.pipe(cp.stdin);
+  cp.stdout.pipe(stdout);
+};
+  
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
+  
